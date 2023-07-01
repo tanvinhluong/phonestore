8edaa7e074b6ea7f1375b1f1526eec02a865bbbb
Luong Tan Vinh <94514193+tanvinhluong@users.noreply.github.com>
2023-07-01 13:05:17 +0700

    Add files via upload

---
+++ src/quanlicuahangdienthoai/Quanlicuahangdienthoai.java
@@ -0,0 +1,13 @@
+
+package quanlicuahangdienthoai;
+
+
+public class Quanlicuahangdienthoai {
+
+   
+    public static void main(String[] args) {
+       System.setProperty("sun.java2d.uiScale", "1.0");
+       new MainPage().setVisible(true);
+    }
+    
+}
